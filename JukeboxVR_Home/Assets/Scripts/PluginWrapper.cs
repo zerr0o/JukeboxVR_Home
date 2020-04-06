ad85a7bd3eadc94b7bd95d4dde1bd42cbc87013c
zerr0o <vhsperes@gmail.com>
2020-04-06 17:56:26 +0200

    Add constants for commands

--- JukeboxVR_Home/Assets/Scripts/PluginWrapper.cs
+++ JukeboxVR_Home/Assets/Scripts/PluginWrapper.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.Events;
 
 public class FromService
 {
     public string command = "";
+    public List<string> parameters = new List<string>();
     public int duration;
     public bool test;
-    
-
 }
 
 
@@ -19,6 +17,17 @@ public class ToService
     public int timeremaining = 0;
     public string nameOfApp = "appplicationname";
     public bool readyToStart = false;
+    public int isPlaying = 0;
+    public bool isFinished = false;
+}
+
+
+public class constant
+{
+    public static string CMD_LOAD_SESSION = "CMD_LOAD_SESSION";
+    public static string CMD_STOP_SESSION = "CMD_STOP_SESSION";
+    public static string CMD_START_SESSION = "CMD_START_SESSION";
+    public static string CMD_CHECK_FILES = "CMD_CHECK_FILES";
 }
 
 public class PluginWrapper : MonoBehaviour
@@ -27,10 +36,16 @@ public class PluginWrapper : MonoBehaviour
 
     private AndroidJavaObject javaClass;
     private float timer = 0;
+    public UnityEvent OnReady;
+    public UnityEvent OnStart;
+    public UnityEvent OnStop;
+
+    public List<string> parametersReceived = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
         javaClass = new AndroidJavaObject("com.JukeboxVR.broadcastunity.BroadCastUnityClass");
         Debug.LogError("JAVACLASS = " + javaClass.ToString());
         javaClass.CallStatic("createInstance");
@@ -42,7 +57,7 @@ public class PluginWrapper : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        /*timer += Time.deltaTime;
         if (timer > 2)
         {
 
@@ -57,6 +72,7 @@ public class PluginWrapper : MonoBehaviour
             //Debug.LogError(javaClass.Call<int>("ReturnnumberToUnity", Time.timeSinceLevelLoad));
             timer = 0;
         }
+        */
 
 
     }
@@ -67,13 +83,22 @@ public class PluginWrapper : MonoBehaviour
         //Debug.LogError("externalConfigurationRaw = " + externalConfigurationRaw);
         Debug.LogError("message received : " + message);
         FromService msg = JsonUtility.FromJson<FromService>(message);
-        
-        if ( msg.command == "CMD_LOAD_SESSION" )
+        if (msg.command == constant.CMD_LOAD_SESSION)
         {
+            parametersReceived = msg.parameters;
+            OnReady.Invoke();
             sendReady();
         }
+        else if (msg.command == constant.CMD_START_SESSION)
+        {
+            OnStart.Invoke();
+        }
+        else if (msg.command == constant.CMD_STOP_SESSION)
+        {
+            OnStop.Invoke();
+        }
 
-        
+
         //foreach ( string command in myObject.commands )
         //{
         //    Debug.LogError(command);
@@ -91,6 +116,17 @@ public class PluginWrapper : MonoBehaviour
         SendMessageToService(msgToService);
     }
 
+    public void sendInfo(int timeremaining, int isPlaying, bool finished)
+    {
+        ToService msgToService = new ToService();
+        msgToService.readyToStart = false;
+        msgToService.timeremaining = timeremaining;
+        msgToService.isPlaying = isPlaying;
+        msgToService.isFinished = finished;
+        Debug.LogError("timeremaining = " + msgToService.timeremaining);
+        SendMessageToService(msgToService);
+    }
+
 
     public void SendMessageToService(ToService msgToService)
     {
@@ -100,7 +136,7 @@ public class PluginWrapper : MonoBehaviour
         AndroidJavaClass androidJC = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
         AndroidJavaObject activity = androidJC.GetStatic<AndroidJavaObject>("currentActivity");
         javaClass.Call("sendIntent", new object[] { jsonMsg.ToString(), activity });
-        
+
     }
 
 
@@ -117,5 +153,5 @@ public class PluginWrapper : MonoBehaviour
     }
 
 
-    
+
 }
